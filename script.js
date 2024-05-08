0f808a4ebf3e1e96e632915c72400026ab244761
HelenCristina19 <helensouzza8@gmail.com>
2024-05-08 14:46:09 +0000

    Projeto finalizado

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ function inserir(num) {
 
 function clean() {
     document.querySelector(".tela").innerHTML = "";
-
 }
 
-function back () {
-    
+function back() {
+    let tela = document.querySelector('.tela').innerHTML;
+    document.querySelector(".tela").innerHTML = tela.substring(
+        0, 
+        tela.length - 1
+    );
 }
 
+function somar() {
+    let tela = document.querySelector('.tela').innerHTML;
+    if(tela) {
+      document.querySelector(".tela").innerHTML = eval(tela);
+    } else{
+      document.querySelector('.tela').innerHTML = "nenhum comando..."
+    }
+}
